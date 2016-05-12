732f9430b2ac76b56a35575f2f2d9d3775cbc675
jfruitet <jean.fruitet@free.fr>
2016-05-12 12:48:43 +0200

    Initial commit
    Signed-off-by: jfruitet <jean.fruitet@free.fr>

---
+++ README.md
@@ -0,0 +1,46 @@
+lpimportcsv
+===========
+
+## English
+
+This admin tool allows import/export of competency frameworks using a
+comma separated value file (CSV).
+
+This imports and exports all data contained in the competency
+framework including related competencies, and any configured
+competency rules.
+
+### Installation
+
+* Download the zip archive
+* Rename it  "lpimportcsv"
+* Copy it in
+
+	./moodle/admin/tool directory
+
+* Log in as "admin"
+* Go to "Site administration / Notification"
+* Update the Moodle database.
+
+That's all!
+
+## Français
+
+Cet outil d'administration permet l'importation / exportation de référentiels de compétences à partir d'un fichier CSV avec séparateur virgule (',').
+
+Il importe et exporte toutes les données des tables mdl_competency_xxx, y compris
+les compétences associées, les barèmes et les règles de validation.
+
+### Installation
+
+* Télécharger l'archive ZIP
+* La renommer "lpimportcsv"
+* Copier tout le contenu dans le dossier
+
+	./moodle/admin/tool directory
+
+* Se loger comme "adminisitrateur"
+* Passez par "Administration du site / Notification"
+* Mettre à jour la base de données.
+
+C'est tout !
---
+++ lang/fr/tool_lpimportcsv.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tool_lpimportau', language 'fr'
+ *
+ * @package    tool_lp
+ * @copyright  2015 Damyon Wiese
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Importer un référentiel de compétences';
+$string['exportnavlink'] = 'Exporter un référentiel de compétence';
+$string['importfile'] = 'Fichier référentiel au format CSV';
+$string['invalidimportfile'] = 'Format de fichier invalide.';
+$string['noframeworks'] = 'Aucun référentiel de compétences n\'a été créé jusqu\'à présent';
+$string['export'] = 'Exporter';
+$string['import'] = 'Importer';
+
+$string['parentidnumber'] = 'N° d\'identifiant de la compétence parente';
+$string['idnumber'] = 'Identifiant (ce code doit être unique pour le référentiel)';
+$string['shortname'] = 'Nom court';
+$string['description'] = 'Description';
+$string['descriptionformat'] = 'Format de description';
+$string['scalevalues'] = 'Valeurs du barème';
+$string['scaleconfiguration'] = 'Configuration du barème';
+$string['ruletype'] = 'Type de règle (optionel)';
+$string['ruleoutcome'] = 'Rule outcome (optionel)';
+$string['ruleconfig'] = 'Configuration de la règle (optionel)';
+$string['isframework'] = 'Is framework';  // mot clé à ne pas modifier !
+$string['isframework_help'] = 'C\'est un référentiel de compétences';
+$string['taxonomy'] = 'Taxonomie';
+$string['competencyscale'] = 'Barème de compétence : {$a}';
+$string['exportid'] = 'Identifiant exporté (optionel)';
+$string['competencyscaledescription'] = 'Barème créé à l\'importation ';
+$string['relatedidnumbers'] = 'Identifiants des compétences associées ';
+$string['confirm'] = 'Confirmer';
+$string['confirmcolumnmappings'] = 'Confirmer l\'affectation des noms de colonnes';
+$string['csvdelimiter'] = 'Délimiteur CSV';
+$string['csvdelimiter_help'] = 'Séparateur des champs du fichier CSV (en général c\'est la virgule \',\' pour ce type de fichier).';
+$string['encoding'] = 'Encodage';
+$string['encoding_help'] = 'Format d\'encodage du fichier CSV (en général Moodle utilise UTF-8).';
